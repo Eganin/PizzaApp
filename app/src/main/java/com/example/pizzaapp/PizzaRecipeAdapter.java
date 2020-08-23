8679a6efb61fabb7772c390db9f91c27c3fe48f6
Eganin <egorzaharin12@gmail.com>
2020-08-24 02:50:33 +0500

    add recycler list and basic functional

--- app/src/main/java/com/example/pizzaapp/PizzaRecipeAdapter.java
+++ app/src/main/java/com/example/pizzaapp/PizzaRecipeAdapter.java
@@ -1,4 +1,67 @@
 package com.example.pizzaapp;
 
-public class PizzaRecipeAdapter {
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class PizzaRecipeAdapter
+        extends RecyclerView.Adapter<PizzaRecipeAdapter.PizzaRecipeViewHolder> {
+
+    ArrayList<PizzaRecipeItem> pizzaRecipeItems;
+
+    public PizzaRecipeAdapter(ArrayList<PizzaRecipeItem> arrayList){
+        pizzaRecipeItems = arrayList;
+    }
+
+    public static class PizzaRecipeViewHolder extends RecyclerView.ViewHolder{
+        /*
+        Наполняем подкласс данными
+        для заполнения элемента
+         */
+        public ImageView imageView;
+        public TextView textView1;
+        public TextView textView2;
+
+        public PizzaRecipeViewHolder(@NonNull View itemView) {
+            super(itemView);
+            this.imageView = itemView.findViewById(R.id.pizzaImageView);
+            this.textView1 = itemView.findViewById(R.id.titleImageView);
+            this.textView2 = itemView.findViewById(R.id.descriptionImageView);
+        }
+    }
+
+    @NonNull
+    @Override
+    public PizzaRecipeViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        /*
+        если требуется создается новый элемент
+         */
+        View view = LayoutInflater.from(parent.getContext()).
+                inflate(R.layout.pizza_recipe_item, parent, false);// указываем id карточки
+
+        PizzaRecipeViewHolder pizzaRecipeViewHolder = new PizzaRecipeViewHolder(view);
+        return pizzaRecipeViewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull PizzaRecipeViewHolder holder, int position) {
+        PizzaRecipeItem pizzaRecipeItem = pizzaRecipeItems.get(position);
+
+        holder.imageView.setImageResource(pizzaRecipeItem.getImageResource());
+        holder.textView1.setText(pizzaRecipeItem.getTitle());
+        holder.textView2.setText(pizzaRecipeItem.getDescription());
+    }
+
+    @Override
+    public int getItemCount() {
+        return pizzaRecipeItems.size();
+    }
+
 }
--- app/src/main/java/com/example/pizzaapp/PizzaRecipeItem.java
+++ app/src/main/java/com/example/pizzaapp/PizzaRecipeItem.java
@@ -1,4 +1,48 @@
 package com.example.pizzaapp;
 
 public class PizzaRecipeItem {
+
+    private int imageResource;
+    private String title;
+    private String description;
+    private String recipe;
+
+    public PizzaRecipeItem(int imageResource, String title, String description , String recipe) {
+        this.imageResource = imageResource;
+        this.title = title;
+        this.description= description;
+        this.recipe = recipe;
+    }
+
+    public String getRecipe() {
+        return recipe;
+    }
+
+    public void setRecipe(String recipe) {
+        this.recipe = recipe;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public int getImageResource() {
+        return imageResource;
+    }
+
+    public void setImageResource(int imageResource) {
+        this.imageResource = imageResource;
+    }
 }
